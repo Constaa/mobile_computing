18ccf0ebadad74c57fe14780ffae5fa6a8ac2200
Fabian Schelble <Fabian.Schelble@durr.com>
2024-11-14 08:04:19 +0100

    Backend: Add example code demonstrating a MariaDB access.

--- calendar-backend/Program.cs
+++ calendar-backend/Program.cs
@@ -1,4 +1,8 @@
 
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+
 namespace calendar_backend
 {
     public class Program
@@ -27,6 +31,9 @@ namespace calendar_backend
 
             app.UseAuthorization();
 
+
+            #region routes
+
             app.MapGet("/test", (HttpContext httpContext) =>
             {
                 return TypedResults.Ok(true);
@@ -40,6 +47,77 @@ namespace calendar_backend
             })
             .Produces<bool>(StatusCodes.Status200OK);
             //.RequireAuthorization();
+
+            //////////////////
+
+            app.MapGet("/mariaDBExample", (HttpContext httpContext) =>
+            {
+                //Used to not actually execute the function, as the underlying MariaDB does not exist.
+                return StatusCodes.Status501NotImplemented;
+
+                //Demo
+                List<string> list = new();
+
+                try
+                {
+                    using (var mysqlconnection = new MySqlConnection("CONNECTION_STRING"))
+                    {
+                        mysqlconnection.Open();
+                        using (MySqlCommand cmd = mysqlconnection.CreateCommand())
+                        {
+                            cmd.CommandType = CommandType.Text;
+                            cmd.CommandTimeout = 300;
+                            cmd.CommandText = "Select * From tokens Where user=@user";
+                            cmd.Parameters.AddWithValue("@user", "USER_NAME");
+
+                            MySqlDataReader reader = cmd.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    {
+                                        if (i < 2)
+                                        {
+                                            list.Add(reader.GetString(i));
+                                        }
+                                        else
+                                        {
+                                            list.Add(reader.GetDateTime(i).ToString());
+                                        }
+                                    }
+                                }
+                            }
+                            reader.Close();
+                            mysqlconnection.Close();
+                        }
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    //return false;
+                }
+                catch (Exception ex)
+                {
+                    //return false;
+                }
+                finally
+                {
+
+                }
+
+            })
+            .WithName("MariaDB Example")
+            .WithTags("System")
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Demonstration of a MariaDB access call",
+                Description = "Function that demonstrates how data from MariaDB can be accessed."
+            })
+            .Produces<bool>(StatusCodes.Status200OK);
+            //.RequireAuthorization();
+
+            #endregion
 
             app.Run();
         }
