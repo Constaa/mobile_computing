5a6d49364e943504c802199c27967c24d2aac70f
Fabian Schelble <Fabian.Schelble@durr.com>
2024-11-14 11:31:47 +0100

    Frontend: Add initial version of dialog for adding new events

---
+++ calendar-frontend/src/components/add-event-dialog/add-event-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { AdminComponent } from '../admin/admin.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, NativeDateAdapter, provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'app-add-event-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule, FormsModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
+  providers: [provideNativeDateAdapter()],
+  templateUrl: './add-event-dialog.component.html',
+  styleUrl: './add-event-dialog.component.scss'
+})
+export class AddEventDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<AdminComponent>);
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+}
--- calendar-frontend/src/components/admin/admin.component.ts
+++ calendar-frontend/src/components/admin/admin.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { AddEventDialogComponent } from '../add-event-dialog/add-event-dialog.component';
 
 @Component({
   selector: 'app-admin',
@@ -11,5 +13,15 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  readonly dialog = inject(MatDialog);
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddEventDialogComponent, { autoFocus: false });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        //TODO: use input data to create a new entry in the database by sending the data via the service
+      }
+    })
+  }
 }
