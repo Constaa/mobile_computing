c9aae97e41d718211daf867f5f9e41c393b26834
Fabian Schelble <Fabian.Schelble@durr.com>
2024-11-18 15:33:37 +0100

    Backend: Add initial test events

---
+++ calendar-backend/CalendarEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace calendar_backend
+{
+    public class CalendarEvent
+    {
+        public int Id { get; set; }
+        public bool AllDay { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public List<int> DaysOfWeek { get; set; }
+        public string Title { get; set; }
+    }
+}
--- calendar-backend/Program.cs
+++ calendar-backend/Program.cs
@@ -11,6 +11,18 @@ namespace calendar_backend
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddCors(options =>
+                options.AddPolicy(
+                  name: "CorsPolicy",
+                  builder =>
+                  {
+                      builder.AllowAnyMethod()
+                      .AllowAnyHeader()
+                      .AllowCredentials()
+                      .WithOrigins("http://localhost:4200");
+                  })
+            );
+
             // Add services to the container.
             builder.Services.AddAuthorization();
 
@@ -27,14 +39,57 @@ namespace calendar_backend
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseHttpsRedirection();
+            }
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
 
             #region routes
+            app.MapGet("api/getEvents", (HttpContext httpContext) =>
+            {
+                //Samples for testing
+                //TODO: Get events from MariaDB
+                List<CalendarEvent> events = [
+                    new CalendarEvent() {
+                        Id = 1,
+                        AllDay = false,
+                        Start = DateTime.Now,
+                        End = DateTime.Now.AddHours(3),
+                        Title = "First test event"
+                    }, new CalendarEvent() {
+                        Id = 2,
+                        AllDay = true,
+                        Start = DateTime.Now.AddDays(4),
+                        End = DateTime.Now.AddDays(4),
+                        Title = "Second Test event"
+                    },
+                    new CalendarEvent() {
+                        Id = 3,
+                        AllDay = false,
+                        Start = DateTime.Now.AddDays(4),
+                        End = DateTime.Now.AddDays(6),
+                        Title = "Third Test event"
+                    }
+                ];
 
-            app.MapGet("/test", (HttpContext httpContext) =>
+
+                return TypedResults.Ok(events);
+            })
+            .WithName("Get events")
+            .WithTags("Event")
+            .WithOpenApi(operation => new(operation)
+            {
+                Summary = "Get calendar events",
+                Description = "Function for getting event data from the database."
+            })
+            .Produces<List<CalendarEvent>>(StatusCodes.Status200OK);
+
+            app.MapGet("api/test", (HttpContext httpContext) =>
             {
                 return TypedResults.Ok(true);
             })
@@ -50,7 +105,7 @@ namespace calendar_backend
 
             //////////////////
 
-            app.MapGet("/mariaDBExample", (HttpContext httpContext) =>
+            app.MapGet("api/mariaDBExample", (HttpContext httpContext) =>
             {
                 //Used to not actually execute the function, as the underlying MariaDB does not exist.
                 return StatusCodes.Status501NotImplemented;
