8d6a9f6f0dcd1f2d204151a936002e4ce49aff59
Fabian Schelble <Fabian.Schelble@durr.com>
2024-12-04 14:20:47 +0100

    Backend: fix erronous semicolon.

--- calendar-backend/Program.cs
+++ calendar-backend/Program.cs
@@ -57,7 +57,7 @@ namespace calendar_backend
             #region routes
             app.MapGet("api/getEvents", (HttpContext httpContext) =>
             {
-                ; List<CalendarEvent> list = [];
+                List<CalendarEvent> list = [];
 
                 try
                 {
