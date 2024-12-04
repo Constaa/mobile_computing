7f6a63765df1b3108aa353697d0b3ab5df39739a
Fabian Schelble <schelblefabian@gmail.com>
2024-12-04 15:19:23 +0100

    General: Update MariaDB state

--- mariadb.sql
+++ mariadb.sql
@@ -37,18 +37,27 @@ CREATE TABLE IF NOT EXISTS `calendarevents` (
   `startTime` time DEFAULT NULL,
   `endTime` time DEFAULT NULL,
   KEY `Schlüssel 1` (`Id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- Exportiere Daten aus Tabelle mobilecomputing.calendarevents: ~7 rows (ungefähr)
+-- Exportiere Daten aus Tabelle mobilecomputing.calendarevents: ~8 rows (ungefähr)
 INSERT INTO `calendarevents` (`Id`, `allDay`, `start`, `end`, `daysOfWeek`, `title`, `className`, `description`, `minParticipants`, `maxParticipants`, `_year`, `startRecur`, `endRecur`, `startTime`, `endTime`) VALUES
-	(5, 0, '2024-11-19 15:09:26', '2024-11-19 17:09:26', '', 'First event', '', 'Description1', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(6, 1, '2024-11-23 15:09:26', '2024-11-23 17:09:26', '', 'Second event', '', 'Description2', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(7, 0, '2024-11-23 15:09:26', '2024-11-25 17:09:26', '', 'Third event', '', '', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(8, 0, '2024-11-25 15:37:54', '2024-11-25 18:37:54', '', 'Fourth event', '', '', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(9, 0, '2024-11-29 13:00:00', '2024-11-30 16:00:00', '', 'New Test event', '', 'Description for test event', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(10, 0, '2024-12-02 05:00:00', '2024-12-03 11:00:00', '', 'Test neu', '', 'Beschreibung', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(11, 0, '2024-12-03 05:00:00', '2024-12-03 06:00:00', '', 'Neues Test event', '', 'das ist die Beschreibung', 0, 0, '2024', NULL, NULL, NULL, NULL),
-	(14, 0, '2024-12-02 05:00:00', '2024-12-23 09:00:00', '1,2', 'Wiederholung', '', 'Test1', 0, 0, '2024', '2024-12-02 05:00:00', '2024-12-23 09:00:00', '06:00:00', '10:00:00');
+	(5, 0, '2024-11-19 15:09:26', '2024-11-19 17:09:26', '', 'First event', 'seminar', 'Description1', 0, 0, '2024', NULL, NULL, NULL, NULL),
+	(6, 1, '2024-11-23 15:09:26', '2024-11-23 17:09:26', '', 'Second event', 'course', 'Description2', 0, 0, '2024', NULL, NULL, NULL, NULL),
+	(9, 0, '2024-11-29 13:00:00', '2024-11-30 16:00:00', '', 'New Test event', 'conference', 'Description for test event', 0, 0, '2024', NULL, NULL, NULL, NULL),
+	(10, 0, '2024-12-02 05:00:00', '2024-12-03 11:00:00', '', 'Test neu', 'conference', 'Beschreibung', 0, 0, '2024', NULL, NULL, NULL, NULL),
+	(11, 0, '2024-12-03 05:00:00', '2024-12-03 06:00:00', '', 'Neues Test event', 'seminar', 'das ist die Beschreibung', 0, 0, '2024', NULL, NULL, NULL, NULL),
+	(14, 0, '2024-12-02 05:00:00', '2024-12-23 09:00:00', '1,2', 'Wiederholung', 'course', 'Test1', 0, 0, '2024', '2024-12-02 05:00:00', '2024-12-23 09:00:00', '06:00:00', '10:00:00'),
+	(15, 0, '2024-12-05 07:00:00', '2024-12-05 11:30:00', '', 'Test mit Teilnehmeranzahjl', 'seminar', 'fwAEF SAE', 5, 20, '2024', NULL, NULL, NULL, NULL),
+	(16, 0, '2024-12-06 02:00:00', '2024-12-06 06:00:00', '', 'Neues Event', 'course', 'Neue Beschreibung', 1, 10, '2024', NULL, NULL, NULL, NULL);
+
+-- Exportiere Struktur von Trigger mobilecomputing.insert_year_trigger
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `insert_year_trigger` BEFORE INSERT ON `calendarevents` FOR EACH ROW BEGIN
+SET NEW._year = YEAR(NEW.start);
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
